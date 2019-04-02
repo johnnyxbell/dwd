b61f59adee10c6ee8be8df78d12b4684af53cab6
Johnny Bell <johnny@stackshare.io>
2019-04-01 18:09:44 -0700

    added some more features

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components/macro';
+
+const Navigation = styled.nav`
+  padding: 0;
+  margin: 0;
+  ul {
+    display: flex;
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    list-style-type: none;
+  }
+`;
 
 const Nav = () => {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/speak/">Speak</Link>
-          </li>
-          <li>
-            <Link to="/sponsor/">Sponsor</Link>
-          </li>
-          <li>
-            <Link to="/contact/">Contact Us</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <Navigation>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/speak/">Speak</Link>
+        </li>
+        <li>
+          <Link to="/sponsor/">Sponsor</Link>
+        </li>
+        <li>
+          <Link to="/contact/">Contact Us</Link>
+        </li>
+      </ul>
+    </Navigation>
   );
 };
 
