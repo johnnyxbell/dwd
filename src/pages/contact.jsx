b13039eece8fafacb2d0ae53d5b4e486b74b670e
Johnny Bell <johnny@stackshare.io>
2019-04-02 12:33:58 -0700

    added form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/layout/Layout';
+import LayoutPage from '../components/layout/layout-page';
 import Heading from '../library/headings/Heading';
 import { Helmet } from 'react-helmet';
 
@@ -15,9 +15,38 @@ const Contact = () => {
               there that work in tech and have disabilities"
         />
       </Helmet>
-      <Layout>
-        <Heading title="Contact Us" />
-      </Layout>
+      <LayoutPage>
+        <Heading title="Contact us" trail="We would love to hear from you (we don't bite)." />
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Role:{' '}
+              <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </LayoutPage>
     </>
   );
 };
