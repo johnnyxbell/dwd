f2a2e26f661d23f064b025ef31bcd6d5863fc154
Johnny Bell <johnny@stackshare.io>
2019-04-02 20:36:22 -0700

    updated contact us form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
           If you have any general questions you want to ask us, please feel free to use the form below to shoot us
           through a message
         </ParagraphNormal>
-        <form name="contact-us" method="POST" data-netlify="true" action="/">
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
           <FlexSpace>
             <TextInput placeholder="Your First Name" type="text" name="First Name" required={true} />
             <TextInput placeholder="Your Last Name" type="text" name="Last Name" required={true} />
